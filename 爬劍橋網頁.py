265bf150040e408d0144857eeb30e17054643271
FantasyLin <yungyi.cool@gmail.com>
2024-04-08 00:15:13 +0800

    change browser settings from Chrome to Edge

--- 爬劍橋網頁.py
+++ 爬劍橋網頁.py
@@ -1,23 +1,27 @@
-import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
+
+# 設定 Edge 瀏覽器的選項
+edge_options = Options()
+edge_options.add_argument("--headless")  # 啟用無頭模式
+edge_options.add_argument("--window-size=1920x1080")  # 設定視窗大小
+
+# 初始化 Edge WebDriver
+driver = webdriver.Edge(options=edge_options)
+driver.implicitly_wait(2)  # 隱性等待
 
 
-# 設定一些chrome瀏覽器的變數，做了headless(不會開瀏覽器)測試，請確認chrome版本有到60
-chrome_options = Options()
-# chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=1920x1080")
-# 抓取selenium webdriver位置
-driver = webdriver.Edge()
-driver.implicitly_wait(2)  # 隱性等待，使用一次整個程式過程皆有效
-# wait = WebDriverWait(driver, 10)  #顯性等待用，不是所有清況都能用隱性等待解決
+def get_definition(driver, word):
+    driver.get(f"https://dictionary.cambridge.org/zht/詞典/英語-漢語-繁體/{word}")
+    xpath = "/html/body/div[2]/div/div[1]/div[2]/article/div[2]/div[4]/div/div/div/div[3]/div/div[2]/div[1]/div[3]/span"
+    elements = driver.find_elements("xpath", xpath)
+    for i in elements:
+        print(i.text)
 
-driver.get(
-    "https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/architect"
-)
-# xpath = "//span[@class='def-body']/span[@class='trans']"
-xpath = "/html/body/div[2]/div/div[1]/div[2]/article/div[2]/div[4]/div/div/div/div[3]/div/div[2]/div[1]/div[3]/span"
-elements = driver.find_elements("xpath", xpath)
-for i in elements:
-    print(i.text)
+
+get_definition(driver, word="architect")
+get_definition(driver, word="apple")
+get_definition(driver, word="banana")
+get_definition(driver, word="cat")
+get_definition(driver, word="dog")
+get_definition(driver, word="elephant")
